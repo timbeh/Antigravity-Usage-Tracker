c44fd6ad59d651840ad0e69793db0a576edd8466
timbeh <tim.behrends.hilse@gmail.com>
2026-05-01 13:08:17 +0100

    Initial Commit

---
+++ Antigravity Usage Tracker/Antigravity_Usage_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Antigravity_Usage_TrackerApp.swift
+//  Antigravity Usage Tracker
+//
+//  Created by Tim Behrends on 01.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Antigravity_Usage_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
